253e7b017fe6756c9de38b2625fd9edc78170268
dmartin <dmarti19@utk.edu>
2018-12-21 11:40:12 -0500

    added two new classes

---
+++ DataSetDumper/DataGridViewDumper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HamsterLabs.DebugUtilities
+{
+    public static class DataGridViewDumper
+    {
+
+        public static void dumpGridViewRow(DataGridViewRow row)
+        {
+            System.Diagnostics.Debug.WriteLine(Environment.NewLine);
+            if (row.Tag != null)
+                System.Diagnostics.Debug.WriteLine(String.Format("Tag: {0}", row.Tag));
+
+            foreach (DataGridViewCell c in row.Cells)
+            {
+                System.Diagnostics.Debug.WriteLine(String.Format("{0} {1}: {2}", c.ToString(), c.OwningColumn.HeaderText, c.FormattedValue.ToString()));
+            }
+        }
+    }
+}
